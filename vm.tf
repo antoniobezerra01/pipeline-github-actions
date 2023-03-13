6f84793300fb147276d2d8a596ae9c1b635bfed5
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2023-03-13 09:50:22 -0300

    Correção nos workflows

--- vm.tf
+++ vm.tf
@@ -7,10 +7,11 @@ resource "aws_instance" "vm" {
   ami                         = "ami-006dcf34c09e50022"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
-  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
   associate_public_ip_address = true
+
   tags = {
-    Name = "vm-tf-actions"
+    "Name" = "vm-terraform"
   }
 }
